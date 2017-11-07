ef80aead66cb31ea3f7f8a8d3698a8d798329d73
muhammadsaripuddin <aripedward339@gmail.com>
2017-11-07 20:46:23 +0700

    menampilkan hasil gemuk

--- Untitled5.cpp
+++ Untitled5.cpp
@@ -17,4 +17,8 @@ int main () {
  else if  (tinggi/2.5 <= berat <= tinggi/2.3) {
   cout <<  "Hasil : Normal" << endl; 
  }
-
+ 
+ else if (tinggi/2.3 < berat)
+  cout << "Hasil : Gemuk" << endl;
+ 
+}
